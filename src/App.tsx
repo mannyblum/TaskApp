602583752f108275c2aa1d2dd0d4db6bf444bd70
Manny <manny.blum@gmail.com>
2025-05-16 18:33:44 -0400

    changing task array of strings to array of objects to future proof. added id value. basic styles to list item

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 
-const TodoApp = () => {
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [task, setTask] = useState<string>("");
+type Task = {
+  name: string;
+  id: number;
+};
 
-  const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+const TodoApp = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [task, setTask] = useState<Task>({ name: "", id: 0 });
+
+  const handleAddTask = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setTasks((prev) => [...prev, task]);
-    ``;
-    setTask("");
+
+    const newTask: Task = {
+      id: Date.now(),
+      name: task.name,
+    };
+
+    // add new task to array of tasks
+    setTasks([...tasks, newTask]);
+
+    // clear task object
+    setTask({ name: "", id: 0 });
   };
 
   return (
     <div>
-      <form onSubmit={handleAddTask}>
+      <form onSubmit={handleAddTask} className="mb-4">
         <input
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          className="border border-black text-black rounded-sm color p-2 mr-2"
+          value={task.name}
+          onChange={(e) =>
+            setTask((prevTask) => ({ ...prevTask, name: e.target.value }))
+          }
+          className="border border-black text-black rounded-sm p-2 mr-2"
         />
         <button className="text-white bg-indigo-700 rounded-sm p-2 px-4 ">
           Add
@@ -26,8 +41,11 @@ const TodoApp = () => {
       <ul>
         {tasks.map((t, i) => {
           return (
-            <li key={t + "-" + i} className="text-black">
-              {t}
+            <li
+              key={t + "-" + i}
+              className="text-black border border-indigo-900 rounded-sm p-2 mb-2"
+            >
+              {t.name}
             </li>
           );
         })}
