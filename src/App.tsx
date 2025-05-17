23f8a8c39b6883838541f534f7665d409e2f099d
Manny <manny.blum@gmail.com>
2025-05-16 23:08:57 -0400

    adding ability to edit a task

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,121 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import shortUUID from "short-uuid";
+
+import { PencilIcon } from "@primer/octicons-react";
 
 type Task = {
+  id: string;
   name: string;
-  id: number;
+  created: number;
+  updated: number;
+};
+
+const Task_Defaults = {
+  id: "",
+  name: "",
+  created: Date.now(),
+  updated: Date.now(),
+};
+
+type TodoItemProps = {
+  task: Task;
+  onUpdateTask: (task: Task) => void;
+};
+
+const TodoItem = ({ task, onUpdateTask }: TodoItemProps) => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [taskName, setTaskName] = useState<string>("");
+
+  useEffect(() => {
+    setTaskName(task.name);
+  }, [task]);
+
+  const handleEditTask = () => {
+    setEditMode(() => !editMode);
+  };
+
+  useEffect(() => {
+    if (editMode) {
+      if (task.name !== taskName) {
+        onUpdateTask({ ...task, ...{ name: taskName, updated: Date.now() } });
+      }
+    }
+  }, [editMode]);
+
+  return (
+    <li
+      key={task.id}
+      className="text-black border border-indigo-900 rounded-sm p-2 mb-2 flex justify-between items-center"
+    >
+      {!editMode ? (
+        <>{task.name}</>
+      ) : (
+        <input
+          type="text"
+          className="border text-black rounded-sm p-1 "
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+      )}
+      <button
+        onClick={handleEditTask}
+        className={`border rounded-sm p-2 hover:bg-indigo-400 active:bg-indigo-600 hover:text-white focus:outline-1 focus:outline-offset-1 focus:outline-indigo-300`}
+      >
+        <PencilIcon size={24} />
+      </button>
+    </li>
+  );
 };
 
 const TodoApp = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [task, setTask] = useState<Task>({ name: "", id: 0 });
+  const [task, setTask] = useState<Task>(Task_Defaults);
+
+  const shortId = shortUUID.generate();
 
   // TODO Task Management
   //  [x] Add a new Task
-  //  [ ] Edit an existing task
+  //  [x] Edit an existing task
   //  [ ] Delete a task
   //  [ ] Mark a task as complete/incomplete
   //  [ ] View all tasks
   const handleAddTask = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const newTask: Task = {
-      id: Date.now(),
-      name: task.name,
-    };
+    if (task) {
+      const newTask: Task = {
+        id: shortId,
+        name: task.name,
+        created: Date.now(),
+        updated: Date.now(),
+      };
 
-    // add new task to array of tasks
-    setTasks([...tasks, newTask]);
+      // add new task to array of tasks
+      setTasks([...tasks, newTask]);
 
-    // clear task object
-    setTask({ name: "", id: 0 });
+      // clear task object
+      setTask(Task_Defaults);
+    }
+  };
+
+  const handleUpdateTask = (task: Task) => {
+    setTasks((prevTasks) => {
+      const existingTask = prevTasks.find((task) => task.id === task.id);
+
+      if (existingTask && task.updated <= existingTask.updated) {
+        return prevTasks;
+      }
+
+      const updatedTasks = prevTasks.map((tsk) =>
+        tsk.id === task.id ? task : tsk
+      );
+      const isNew = !existingTask;
+      return isNew ? [...prevTasks, task] : updatedTasks;
+    });
   };
 
   return (
-    <div>
+    <div className="w-6/12">
       <form onSubmit={handleAddTask} className="mb-4">
         <input
           value={task.name}
@@ -45,15 +129,8 @@ const TodoApp = () => {
         </button>
       </form>
       <ul>
-        {tasks.map((t, i) => {
-          return (
-            <li
-              key={t + "-" + i}
-              className="text-black border border-indigo-900 rounded-sm p-2 mb-2"
-            >
-              {t.name}
-            </li>
-          );
+        {tasks.map((tsk) => {
+          return <TodoItem onUpdateTask={handleUpdateTask} task={tsk} />;
         })}
       </ul>
     </div>
